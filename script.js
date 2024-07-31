2ba90e411b5c769448d669618f7d15e4dde54bf9
Err-Ye <146213709+Err-Ye@users.noreply.github.com>
2024-07-31 22:59:50 +0630

    adding_some_js

--- script.js
+++ script.js
@@ -1,16 +1,66 @@
-let question = [];
-let answer = 0;
+let question = [];//for formula (get input)
+let answer = 0;//for answer 
+let fromulaDisplay = document.getElementById('formula');//for show formula
 
+/**
+ * Description : for adding number to array and show them in display
+ * Created : 31.7.2024
+ * Created by : Ye Htet Aung
+ * Updated by :
+ * parameter : e for get the input data
+ */
 let addingNum = (e) => {
     question.push(e.id);
     console.log(question);
+    showingFormula(question);
 }
 
+
+/**
+ * Description : for adding operator to array and show them in display
+ * Created : 31.7.2024
+ * Created by : Ye Htet Aung
+ * Updated by :
+ * parameter : e for get the input data
+ */
 let addingOperator = (e) => {
     question.push(e.id);
     console.log(question);
+    showingFormula(question);
+
 }
 
+/**
+ * Description : for show array in display
+ * Created : 31.7.2024
+ * Created by : Ye Htet Aung
+ * Updated by :
+ * parameter : e for array (the formula array)
+ */
+let showingFormula = (e) => {
+    fromulaDisplay.innerText=e.join('');
+}
+
+/**
+ * Description : for adding number to array and show them in display
+ * Created : 31.7.2024
+ * Created by : Ye Htet Aung
+ * Updated by :
+ * parameter : e for get the input data
+ */
 let showingPreview = (array) => {
     let preview = document.getElementById('preview');
+}
+
+/**
+ * Description : for rest
+ * Created : 31.7.2024
+ * Created by : Ye Htet Aung
+ * Updated by :
+ * parameter : 
+ */
+let reset = () => {
+    question = [];
+    answer = 0;
+    showingFormula(question);
 }
